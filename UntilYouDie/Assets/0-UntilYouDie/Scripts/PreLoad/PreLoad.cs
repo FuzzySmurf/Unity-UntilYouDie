25a8837aab61db52c458954c91671864f210fb89
AlexMoreno <alexmoreno777@gmail.com>
2017-03-20 16:56:39 -0700

    First Scene to start.

---
+++ UntilYouDie/Assets/0-UntilYouDie/Scripts/PreLoad/PreLoad.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PreLoad : MonoBehaviour {
+
+    void Start () {
+        StartCoroutine(loadFirstScene());
+    }
+
+    public IEnumerator loadFirstScene() {
+        yield return new WaitForSeconds(5.0f);
+        SceneManager.LoadScene("MainMenu");
+    }
+}
